50644dfad37f808662a3586078829c069e4c0f04
Upendra Singh <35781726+upendrasingh12@users.noreply.github.com>
2020-12-19 20:04:10 +0530

    write comments

--- hello_world.dart
+++ hello_world.dart
@@ -1,7 +1,9 @@
 void main() {
   
-  print("Hello World");
-  print("This is my first application");
-  print(12);
-  print(2020 - 1995);
+  
+  //This is my first line code
+  print("Hello World"); //string
+  print("This is my first application"); //string
+  print(12); //integer
+  print(2020 - 1995); //subtraction
 }
